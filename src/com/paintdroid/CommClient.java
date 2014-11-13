503f69b6aff0d35437df8f5b800a36c9f87239b4
NormantasKudzma <kudzma.normantas@gmail.com>
2014-11-13 14:41:12 +0200

    Moved ip/port to settings file. Minor client fixes

--- src/com/paintdroid/CommClient.java
+++ src/com/paintdroid/CommClient.java
@@ -1,17 +1,47 @@
 package com.paintdroid;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import android.os.Environment;
 import android.util.Log;
 
 public class CommClient implements Runnable{
-	public static final int PORT = 60210;
-	public static final String IP = "172.16.143.205";
-//	public static final String IP = "192.168.1.74";
+	protected enum SetVar {
+		VARIP {
+			void set(String s){
+				CommClient.IP = s;
+			}
+			String get(){
+				return CommClient.IP;
+			}
+		},
+		VARPORT {
+			void set(String s){
+				CommClient.PORT = Integer.parseInt(s);
+			}
+			Integer get(){
+				return CommClient.PORT;
+			}
+		};
+		
+		abstract void set(String s);
+		abstract Object get();
+	}
+	
+	public final static String DEF_PATH = Environment.getExternalStorageDirectory().toString();
+	public final static String FILE_NAME = "CommClientSettings.dat";
+	
+	static int PORT = 60210;
+	static String IP = "192.168.1.74";
 	
 	Socket socket;
 	PrintWriter writer;
@@ -24,6 +54,7 @@ public class CommClient implements Runnable{
 	
 	public CommClient(Design d){
 		design = d;
+		loadSettings(DEF_PATH + "/" + FILE_NAME);	
 	}
 	
 	@Override
@@ -86,5 +117,66 @@ public class CommClient implements Runnable{
 			stopClient();
 		}
 		return isConnected;
+	}
+
+	private void initSettings(String args){
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(design.getAssets().open(FILE_NAME)));
+			loadSettings(br);
+			saveSettings(args);
+			br.close();
+		}
+		catch (Exception e){
+			Log.e("CommClient", "Error initializing settings " + e.getMessage());
+		}
+	}
+	
+	private void loadSettings(BufferedReader br){
+		try {
+			String line;
+			while ((line = br.readLine()) != null){
+				try {
+					String st = line.substring(0, line.indexOf('='));
+					String end = line.substring(line.indexOf('=') + 1);
+					Log.d("CommClient", "Read parameter : " + st + " ; value : " + end);
+					SetVar.valueOf(st).set(end);
+				}
+				catch (Exception e){
+					e.printStackTrace();
+				}			
+			}
+			br.close();
+		}
+		catch (Exception e){
+			Log.e("CommClient", "Error loading settings " + e.getMessage());
+			initSettings(DEF_PATH + "/" + FILE_NAME);
+		}
+	}
+	
+	private void loadSettings(String args){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(args)));
+			loadSettings(br);
+			br.close();
+		}
+		catch (Exception e){
+			Log.e("CommClient", "Error loading settings " + e.getMessage());
+			initSettings(DEF_PATH + "/" + FILE_NAME);
+		}
+	}
+	
+	private void saveSettings(String args){
+		try {
+			Log.d("CommClient", "Writing to settings file..");
+			BufferedWriter br = new BufferedWriter(new FileWriter(new File(args)));
+			br.write(SetVar.VARIP.name() + "=" + IP + "\n");
+			br.write(SetVar.VARPORT.name() + "=" + PORT);
+			br.flush();
+			br.close();
+			Log.d("CommClient", "Writing - success.");
+		}
+		catch (Exception e){
+			Log.e("CommClient", "Error saving settings " + e.getMessage());
+		}
 	}
 }
